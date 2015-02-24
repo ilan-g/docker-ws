26776c146e90850a2c50e7c45c123680a09b3236
kris-g98 <compilan@yahoo.co.in>
2015-02-24 15:46:41 +0530

    Added new unit test

---
+++ src/test/java/com/span/service/EmployeeServiceTest.java
@@ -0,0 +1,14 @@
+package com.span.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmployeeServiceTest {
+
+	@Test
+	public void testGetEmployeeList() {
+		fail("Not yet implemented");
+	}
+
+}
