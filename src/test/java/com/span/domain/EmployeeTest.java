de438426c643354c5306e7863207a89584ef1f51
kris-g98 <compilan@yahoo.co.in>
2015-05-07 10:50:56 +0530

    TG-1 #ready-for-test

---
+++ src/test/java/com/span/domain/EmployeeTest.java
@@ -0,0 +1,31 @@
+package com.span.domain;
+
+import static org.junit.Assert.*;
+
+import com.span.domain.Employee;
+
+import org.junit.Test;
+
+public class EmployeeTest {
+
+	private static final int AGE = 23;
+	private static final String NAME = "ilan";
+
+	@Test
+	public void testGetAge() {
+		Employee emp =  new Employee();
+		emp.setAge(AGE);
+		org.junit.Assert.assertEquals("failure - Age not equal", emp.getAge(), AGE);
+		// fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetName() {
+		Employee emp =  new Employee();
+		emp.setName(NAME);;
+		org.junit.Assert.assertEquals("failure - Age not equal", emp.getName(), NAME);
+		// fail("Not yet implemented");
+	}
+	
+
+}
